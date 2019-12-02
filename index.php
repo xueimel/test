3b7777bf69644529ad76e4345a5bb3242554689f
krtupper <46764559+krtupper@users.noreply.github.com>
2019-12-01 21:41:09 -0700

    Update index.php (#37)
    * Story #22, Implements task #24(Update index.php) [#37]

--- index.php
+++ index.php
@@ -1,34 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-	<link rel="shortcut icon" type="image/png" href="/favicon-16x16.png"/>
-	<link rel="shortcut icon" type="image/png" href="http://localhost:8888/favicon.png"/>
-	<link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-	<div class="topnav">
-  <a class="active" href="./index.html">Home</a>
-  <a href="./index.html">Placeholder</a>
-  <a href="./index.html">Placeholder</a>
-  <a href="./index.html">Placeholder</a>
-  <a href="./index.html">Register Here!</a>
-    <body>
-	</div>
+<?php
+include header.php
+?>
 
         <h1>Welcome To Our Flash Card App!</h1>
-
     <!--
         The student flashcard divs
     -->
         <div id="flashcard-front">
 			<img src="" width="50" height="50" alt="FRONT: Student Picture" />
 		</div>
-
 		<div id="flashcard-back">
 			<h2 id="name">BACK: Student name fetched from db goes here.</h2>
 		</div>
-
 		<button id="flip" onclick="flipCard()">Flip</button>
-
 		<script type="text/javascript">
 			var front = document.getElementById('flashcard-front');
 			var back = document.getElementById('flashcard-back');
@@ -45,7 +29,6 @@
 				}
 			}
 		</script>
-
 		
 		<div class="row">
   <div class="column">
