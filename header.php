113e0a09a6315f6d5d58c54c129973007d9b24fa
Tanner Purves <tannerpurves@u.boisestate.edu>
2019-12-01 21:55:14 -0700

    Update header.php

--- header.php
+++ header.php
@@ -5,9 +5,10 @@
 	<link rel="shortcut icon" type="image/png" href="http://localhost:8888/favicon.png"/>
 	<link rel="stylesheet" type="text/css" href="style.css">
     </head>
+	<body>
 	<div class="topnav">
   <a class="active" href="./index.php">Home</a>
   <a href="./index.php">Choose Existing Class</a>
   <a href="./dragDropFiles.html">Add New Class</a>
   <a href="./login.html">Login</a>
-
+	</div>
