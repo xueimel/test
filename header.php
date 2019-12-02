0c6a1842b752ea0aee9fff8d00c4c4a05159a7d4
krtupper <46764559+krtupper@users.noreply.github.com>
2019-12-01 21:18:46 -0700

    Create header.php (#36)

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	<link rel="shortcut icon" type="image/png" href="/favicon-16x16.png"/>
+	<link rel="shortcut icon" type="image/png" href="http://localhost:8888/favicon.png"/>
+	<link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+	<div class="topnav">
+  <a class="active" href="./index.php">Home</a>
+  <a href="./index.php">Choose Existing Class</a>
+  <a href="./dragDropFiles.html">Add New Class</a>
+  <a href="./login.html">Login</a>
+
