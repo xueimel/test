2ff171812262bd7139ae0c0365455efeaf80ff88
Landon Lemieux <landonlemieux@u.boisestate.edu>
2019-12-12 20:23:39 -0700

    Implements #42 (#53)

--- createNewClass.php
+++ createNewClass.php
@@ -18,14 +18,14 @@ if(isset($_SESSION['upload_message'])) {
 
     <body>
 
-    <h1>Ready to create a class? Start by entering a name.</h1>
+    <h1>Ready to create a class? Enter the student's info below.</h1>
 
     <form action="upload.php" method="post" enctype="multipart/form-data">
         <div class="create_container">
             Select image to upload:
             <input type="file" name="file_to_upload" id="fileToUpload">
-            <input type="text" name="class_name" placeholder="Class Name">
             <input type="text" name="student_name" placeholder="Student Name">
+            <input type="text" name="class_name" placeholder="Class Name">
             <input type="submit" value="Upload Image" name="submit">
         </div>
     </form>
--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-	
 	<link rel="shortcut icon" type="image/png" href="/favicon-16x16.png"/>
 	<link rel="shortcut icon" type="image/png" href="http://localhost:8888/favicon.png"/>
 	<link rel="stylesheet" type="text/css" href="style.css">
@@ -11,26 +10,21 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="/js/form.js" type="text/javascript"></script>
 
-	
-    </head>
-	
-	
-	<body>
-	
-	
-<div class="topnav">
 
+    </head>
+	<body>
+	<div class="topnav">
   <a class="active" href="./index.php">Home</a>
   <a href="./index.php">Choose Existing Class</a>
-  <a href="./dragDropFiles.html">Add New Class</a>
+  <a href="./createNewClass.php">Add Student</a>
   <a id="login"  onclick="document.getElementById('id01').style.display='block'">Login</a>
 
 </div>
-	
-	
+
+
 	<div id="id01" class="modal">
-  
-  
+
+
   <form class="modal-content animate" action="/loginAction.php?a=login" method="post">
     <div class="imgcontainer">
       <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
@@ -43,27 +37,27 @@
 
       <label for="psw"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="psw" required>
-        
+
       <button type="submit">Login</button>
       <label>
         <input type="checkbox" checked="checked" name="remember"> Remember me
       </label>
     </div>
-	
+
     <div class="container" style="background-color:#f1f1f1">
-	
+
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-	 
+
       <span class="psw">Forgot <a href="#">password?</a></span>
      <span class="psw">New User? <a href="signUp.html">Create an account </a></span>
-	
+
     </div>
-	
+
   </form>
-  
+
 </div>
-	
-	
+
+
 
 </body>
 </html>
