ab4809d518c06d74d53c79607caa392d47c66521
allSamGit <35719472+allSamGit@users.noreply.github.com>
2019-12-14 18:23:54 -0700

    remove features (#60)
    Implements #49

--- header.php
+++ header.php
@@ -39,16 +39,13 @@
       <input type="password" placeholder="Enter Password" name="psw" required>
 
       <button type="submit">Login</button>
-      <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-      </label>
+      
     </div>
 
     <div class="container" style="background-color:#f1f1f1">
 
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
 
-      <span class="psw">Forgot <a href="#">password?</a></span>
      <span class="psw">New User? <a href="signUp.html">Create an account </a></span>
 
     </div>
